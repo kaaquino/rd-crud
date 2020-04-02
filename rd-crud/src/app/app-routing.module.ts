404d36bdd8c04f752b23d1a68623983a3be504f7
kaaquino <63032465+kaaquino@users.noreply.github.com>
2020-04-02 01:14:35 -0300

    Add files via upload

---
+++ rd-crud/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { NovoProdutoComponent } from './components/novo-produto/novo-produto.component';
+
+
+const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"/home",
+    pathMatch:"full"
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "sobre",
+    component: SobreComponent
+  },
+  {
+    path: "contato",
+    component: ContatoComponent
+  },
+  {
+    path: "novo-produto",
+    component: NovoProdutoComponent
+  },
+  {
+    path:"**",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
